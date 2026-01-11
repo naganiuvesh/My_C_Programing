8f8c15f00da8d92b40fe71f04bad1d20aa7f6087
uvesh_nagani <164164923+naganiuvesh@users.noreply.github.com>
2026-01-11 19:52:05 +0530

    Add files via upload

---
+++ string_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+        // it can change 
+    char *canChange = "uvesh";  
+    puts(canChange);
+    canChange = "nagani";
+    puts(canChange);
+
+        // it can't change 
+    char canNotChange[] = "uvesh";
+    puts(canNotChange);
+    // canNotChange[] = "nagani";      //   it's not work, you can try.
+    puts(canNotChange);
+
+    return 0;   
+}
